64741f5beb74b9d1fa2ab90536f4bbbc86f44f0b
liuyibao <734988964@qq.com>
2019-03-27 16:01:14 +0800

    add flag

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	_ "database/sql"
+	"flag"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	fmt.Println("this is a test")
+	var isHelp,isInit,isTest bool
+	flag.BoolVar(&isHelp, "h", false, "this help")
+	flag.BoolVar(&isInit, "i", false, "init database")
+	flag.BoolVar(&isTest, "t", false, "test sql")
+	flag.Parse()
+
+	if isHelp {
+		fmt.Println("This is a help message")
+	} else if isInit {
+		fmt.Println("This is a init command")
+	} else if isTest {
+		fmt.Println("This is a test")
+	} else {
+		fmt.Println("other usage")
+	}
 }
