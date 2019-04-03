389ee079f22cc50892376ea2d5f51cc3cc31c69a
liuyibao <734988964@qq.com>
2019-04-03 19:11:05 +0800

    更改表结构

--- model/database.sql
+++ model/database.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `student` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `no` int(11) NOT NULL DEFAULT '0' COMMENT '学号',
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `no` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '学号',
   `name` varchar(100) NOT NULL DEFAULT '' COMMENT '学生姓名',
   `c_score` float NOT NULL DEFAULT '0' COMMENT 'C语言成绩',
   `math_score` float NOT NULL DEFAULT '0' COMMENT '数学成绩',
@@ -10,5 +10,6 @@ CREATE TABLE `student` (
   `ranking` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '排名',
   `updated_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `created_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_no` (`no`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
